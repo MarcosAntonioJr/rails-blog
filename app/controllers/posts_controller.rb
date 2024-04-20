628914472712b59c8505b3325c05675551872850
Marcos Antonio Junior <marcos.junior.profissional@gmail.com>
2024-04-20 18:28:27 -0300

    feat: first commit rails blog

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, except: [:index, :new, :create]
+
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created"
+    else
+      render :new , status: :unprocessable_entity
+
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit , status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+      @post.destroy
+      redirect_to root_path
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
+end
